4a0c46406dbb8b30b9587ae278c7f7165f765d39
serarm <serjesh8735@gmail.com>
2022-03-29 21:32:52 -0400

    Adding excercise of module1

---
+++ udacity_ml_devops_nanodegree/excercises/1_clean_code_principles/l4_production_ready_code/tryexcept.py
@@ -0,0 +1,41 @@
+def divide_vals(numerator, denominator):
+    '''
+    Args:
+        numerator: (float) numerator of fraction
+        denominator: (float) denominator of fraction
+
+    Returns:
+        fraction_val: (float) numerator/denominator
+    '''
+    # try to return the fraction but if the denominator is zero
+    # catch the error and return a string saying: 
+    # "denominator cannot be zero"
+    try:
+        fraction_val = float(numerator/denominator)
+        return fraction_val
+    except ZeroDivisionError:
+        return "denominator cannot be zero"
+        
+    
+def num_words(text):
+    '''
+    Args:
+        text: (string) string of words
+
+    Returns:
+        num_words: (int) number of words in the string
+    '''
+    # try to split based on spaces and return num of words
+    # but if text isnt a string return "text argument must be a string"
+    try:
+        num_words = len(text.split())
+        return num_words
+    except AttributeError:
+        return "text argument must be a string"
+    
+
+if __name__=='__main__':
+    print(divide_vals(5, 2))
+    print(divide_vals(5, 0))
+    print(num_words("This is test"))
+    print(num_words(2000))
